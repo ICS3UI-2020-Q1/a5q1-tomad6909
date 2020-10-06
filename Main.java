4481d16e2d93e43768adbced0fe2611e6f81c8b7
tomad6909 <tomad6909@wrdsb.ca>
2020-10-06 21:17:56 +0000

    Finished A5Q1

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * This program will ask the user for an integer and create a multiplication table for that integer
+ * @author David Toma
  */
 public class Main {
 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask user to input an integer
+    System.out.println("Please enter an integer to create a multiplication table for");
+    int integer = input.nextInt();
+
+    for(int count = 1; count <= 12; count++){
+      int total = count * integer;
+      System.out.println(count + " x " + integer + " = " + total);
+    } 
     
   }
 }
